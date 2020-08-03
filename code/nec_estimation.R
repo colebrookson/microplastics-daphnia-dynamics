c0c9c8411a3f89a35520cf230addda2e4d4fc61b
Cole B. Brookson <brookson@ualberta.ca>
2020-08-02 19:09:20 -0600

    start writing of STAN for NEC estimation

--- code/nec_estimation.R
+++ code/nec_estimation.R
@@ -28,4 +28,44 @@ for(i in unique(offspring$generation)) {
   rm(temp)
 } 
 
+write('
+data {
+  int<lower = 0> N;           
+  real ts[N];                 
+  real y_init[2];             
+  real<lower = 0> y[N, 2];    
+}
+parameters {
+  real<lower = 0> theta[5];   
+  real<lower = 0> z_init[2];  
+  real<lower = 0> sigma[2];   
+}
+transformed parameters {
+  real z[N, 2]
+    = integrate_ode_rk45(dz_dt, z_init, 0, ts, theta,
+                         rep_array(0.0, 0), rep_array(0, 0),
+                         1e-5, 1e-3, 5e2);
+}
+model {
+  theta[{1}] ~ normal(2.5, 1);
+  log(theta[{2}]) ~ normal(0.5,0.5);
+  theta[{3}] ~ normal(35,1);
+  theta[{4, 5}] ~ normal(0.5,0.5);
+  sigma ~ lognormal(-1, 1);
+  z_init ~ lognormal(log(140), 1);
+  for (k in 1:2) {
+    y_init[k] ~ lognormal(log(z_init[k]), sigma[k]);
+    y[ , k] ~ lognormal(log(z[, k]), sigma[k]);
+  }
+}
+generated quantities {
+  real y_init_rep[2];
+  real y_rep[N, 2];
+  for (k in 1:2) {
+    y_init_rep[k] = lognormal_rng(log(z_init[k]), sigma[k]);
+    for (n in 1:N)
+      y_rep[n, k] = lognormal_rng(log(z[n, k]), sigma[k]);
+  }
+}', 
+here('./code/stan_files/nec_estimation.stan'))
 
