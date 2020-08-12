077e41e9e14fd53a8058c33f2c0d1171bad0b8cf
Cole B. Brookson <brookson@ualberta.ca>
2020-08-11 22:30:24 -0600

    add model sampler to NEC stan script

--- code/nec_estimation.R
+++ code/nec_estimation.R
@@ -30,41 +30,26 @@ for(i in unique(offspring$generation)) {
 
 write('
 data {
-  int<lower = 0> N;           
-  real ts[N];                 
-  real y_init[2];             
+  int <lower = 0> N; // sample size    
+  vector[N] x; // concentration                 
+  vector[N] y; // response (death rate)            
   real<lower = 0> y[N, 2];    
 }
+
 parameters {
-  real<lower = 0> theta[5];   
-  real<lower = 0> z_init[2];  
-  real<lower = 0> sigma[2];   
+  real <lower = 0> a; // basal response    
+  real <lower = 0> b; // rate of decay of the response  
+  real <lower = 0> g; // threshold of NEC   
 }
-transformed parameters {
-  real z[N, 2]
-    = integrate_ode_rk45(dz_dt, z_init, 0, ts, theta,
-                         rep_array(0.0, 0), rep_array(0, 0),
-                         1e-5, 1e-3, 5e2);
-}
+
 model {
-  theta[{1}] ~ normal(2.5, 1);
-  log(theta[{2}]) ~ normal(0.5,0.5);
-  theta[{3}] ~ normal(35,1);
-  theta[{4, 5}] ~ normal(0.5,0.5);
-  sigma ~ lognormal(-1, 1);
-  z_init ~ lognormal(log(140), 1);
-  for (k in 1:2) {
-    y_init[k] ~ lognormal(log(z_init[k]), sigma[k]);
-    y[ , k] ~ lognormal(log(z[, k]), sigma[k]);
-  }
+  a ~ gamma(0.0001, 0.0001) // arguments are alpha, beta
+  b ~ gamma(0.0001, 0.0001)
+  g ~ gamma(0.0001, 0.0001)
+  
+  y ~ binomial 
 }
 generated quantities {
-  real y_init_rep[2];
-  real y_rep[N, 2];
-  for (k in 1:2) {
-    y_init_rep[k] = lognormal_rng(log(z_init[k]), sigma[k]);
-    for (n in 1:N)
-      y_rep[n, k] = lognormal_rng(log(z[n, k]), sigma[k]);
   }
 }', 
 here('./code/stan_files/nec_estimation.stan'))
