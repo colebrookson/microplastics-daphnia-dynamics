3095416ad5b5e51e6a20ff38be6b01efa127459e
Cole B. Brookson <brookson@ualberta.ca>
2020-08-25 15:23:23 -0600

    move data prep to correct file

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -28,63 +28,31 @@ offspring =
 offspring = offspring %>% 
   select(-`X1`)
 
-write("
-functions {
-  real[] dz_dt(real t,       
-               real[] z,     
-               real[] theta, 
-               real[] x_r,  
-               int[] x_i) {
-    real P = z[1];
-    real H = z[2];
+### create data groupings
+str(offspring)
+offspring$generation = as.factor(offspring$generation)
+levels(offspring$generation)[levels(offspring$generation)== 'F3'] = 3
 
-    real r = theta[1];  
-    real O = theta[2];
-    real b = theta[3];
-    real c = theta[4];
-    real u = theta[5];
+for(i in unique(offspring$generation)) {
+  
+  temp = offspring[which(offspring$generation == i),]
+  assign(paste('generation_f', i, sep = ''), temp)
+  rm(temp)
+} 
 
-    real dP_dt = P*r - H*(O*P/(1 + O*0.075*P));
-    real dH_dt = b + H*(c*(O*P/(1 + O*0.075*P))-u);
-    return { dP_dt, dH_dt };
-  }
-}
-data {
-  int<lower = 0> N;           
-  real ts[N];                 
-  real y_init[2];             
-  real<lower = 0> y[N, 2];    
-}
-parameters {
-  real<lower = 0> theta[5];   
-  real<lower = 0> z_init[2];  
-  real<lower = 0> sigma[2];   
-}
-transformed parameters {
-  real z[N, 2]
-    = integrate_ode_rk45(dz_dt, z_init, 0, ts, theta,
-                         rep_array(0.0, 0), rep_array(0, 0),
-                         1e-5, 1e-3, 5e2);
-}
-model {
-  theta[{1}] ~ normal(2.5, 1);
-  log(theta[{2}]) ~ normal(0.5,0.5);
-  theta[{3}] ~ normal(35,1);
-  theta[{4, 5}] ~ normal(0.5,0.5);
-  sigma ~ lognormal(-1, 1);
-  z_init ~ lognormal(log(140), 1);
-  for (k in 1:2) {
-    y_init[k] ~ lognormal(log(z_init[k]), sigma[k]);
-    y[ , k] ~ lognormal(log(z[, k]), sigma[k]);
-  }
-}
-generated quantities {
-  real y_init_rep[2];
-  real y_rep[N, 2];
-  for (k in 1:2) {
-    y_init_rep[k] = lognormal_rng(log(z_init[k]), sigma[k]);
-    for (n in 1:N)
-      y_rep[n, k] = lognormal_rng(log(z[n, k]), sigma[k]);
-  }
-}",
-"July2.stan")
+### pull the data and get into proper format
+#note the response is the proportion of surviving/original
+nec_fit_data_f0 = generation_f0 %>% 
+  filter(treatment %in% c('HFC', 'PS_400', 'PS_2000', 'PS_10000'),
+         day == 21) 
+
+nec_fit_data_f0$treatment = as.factor(nec_fit_data_f0$treatment)
+levels(nec_fit_data_f0$treatment)
+levels(nec_fit_data_f0$treatment) = c('0', '10000', '2000', '400')
+nec_fit_data_f0$treatment = 
+  as.numeric(levels(nec_fit_data_f0$treatment))[nec_fit_data_f0$treatment]
+
+nec_fit_data_f0 = nec_fit_data_f0 %>% 
+  select(treatment, survival) %>% 
+  rename(x = treatment, y = survival)
+
